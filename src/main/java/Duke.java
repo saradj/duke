7c889c477276e51d1f2c6d6532ddc937d2d09d34
saradj <sara.djambazovska@epfl.ch>
2019-08-27 22:04:32 +0800

    Add exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -23,7 +23,7 @@ public class Duke {
             System.out.println("\t" + line);
             if (input.equals("list")) {
                 if (tasks.isEmpty())
-                    System.out.println("\t No tasks yet!");
+                    throw new DukeException("No tasks yet!");
                 else {
                     System.out.println("\t Here are the tasks in your list:");
                     for (int i = 1; i <= tasks.size(); i++) { // looping to print all the saved tasks
@@ -31,44 +31,54 @@ public class Duke {
                     }
                 }
             } else {
-                if (input.isBlank())
-                    System.out.println("Please enter a valid task");
-                else {
-                    String[] spllited = input.split(" ", 2);
-                    Task newTask = null;
-
-                    switch (spllited[0]) {
+                String[] splitted = input.split(" ", 2);
+                Task newTask = null;
+                try {
+                    switch (splitted[0]) {
                         case "done":
-                            if (input.length() > 5) {
-                                int taskNb = Integer.parseInt(spllited[1]);
+                            if (splitted.length == 2) {
+                                int taskNb = Integer.parseInt(splitted[1]);
                                 if (taskNb <= tasks.size() && taskNb > 0) {
                                     tasks.get(taskNb - 1).markAsDone();
                                     System.out.println("\t Nice! I've marked this task as done:");
                                     System.out.println("\t " + tasks.get(taskNb - 1).toString());
-                                } else System.out.println("Enter a valid task number");
-                            } else System.out.println("Need to enter a task number after done!");
+                                } else
+                                    throw new DukeException("Enter a valid task number after done, between 1 and " + tasks.size());
+                            } else throw new DukeException("Need a task number after done!");
                             break;
                         case "todo":
-                            newTask = new Todo(spllited[1]);
+                            if ((splitted.length == 1) || splitted[1].isBlank())
+                                throw new DukeException("The description of a todo cannot be empty.");
+                            newTask = new Todo(splitted[1]);
                             break;
                         case "deadline":
-                            String getBy[] = spllited[1].split("/by ", 2);
+                            if ((splitted.length == 1) || splitted[1].isBlank())
+                                throw new DukeException("The description of a deadline cannot be empty.");
+                            String getBy[] = splitted[1].split("/by ", 2);
+                            if (getBy.length < 2)
+                                throw new DukeException("The description of a deadline must contain /by date!");
                             newTask = new Deadline(getBy[0], getBy[1]);
 
                             break;
                         case "event":
-                            String getAt[] = spllited[1].split("/at ", 2);
+                            if ((splitted.length == 1) || splitted[1].isBlank())
+                                throw new DukeException("The description of an event cannot be empty, and it must contain /at");
+                            String getAt[] = splitted[1].split("/at ", 2);
+                            if (getAt.length < 2)
+                                throw new DukeException("The description of a deadline must contain /at data and time from-to!");
                             newTask = new Event(getAt[0], getAt[1]);
                             break;
                         default:
-                            System.out.println("task type invalid!");
+                            throw new DukeException("I'm sorry, but I don't know what that means :-(");
                     }
-                    if (newTask != null) {
-                        tasks.add(newTask);
-                        System.out.println("\t Got it. I've added this task: ");
-                        System.out.println("\t "+ newTask.toString());
-                        System.out.println("\t Now you have " + tasks.size() + " tasks in the list.");
-                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                if (newTask != null) {
+                    tasks.add(newTask);
+                    System.out.println("\t Got it. I've added this task: ");
+                    System.out.println("\t " + newTask.toString());
+                    System.out.println(tasks.size() == 1 ? "\t Now you have " + tasks.size() + " task in the list." : "\t Now you have " + tasks.size() + " tasks in the list.");
                 }
             }
             System.out.println("\t" + line);
