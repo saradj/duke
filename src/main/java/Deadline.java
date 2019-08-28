f10f48f9ff9d767ae858dfc69b493cfadca79a73
saradj <sara.djambazovska@epfl.ch>
2019-08-28 23:53:24 +0800

    Level-8 done

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,39 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
 public class Deadline extends Task {
     protected String by;
+    protected Date date;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        this.date = getDate(by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + by + ")";
+        return (getDate(by) == null) ? "[D]" + super.toString() + "(by: " + by + ")" : "[D]" + super.toString() + "(by: " + getDateString(date) + ")";
     }
 
-    @Override
+    public Date getDate() {
+        return date;
+    }
+
+    private String getDateString(Date date) {
+        if (date == null)
+            return by;
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        String pattern = by.length() > 11 ? "d'" + getDaySuffix(localDate.getDayOfMonth()) + "' 'of' MMMM yyyy, ha " : "d'" + getDaySuffix(localDate.getDayOfMonth()) + "' 'of' MMMM yyyy";
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+        return formatter.format(date);
+    }
+
     public String printInFile() {
-        return this.isDone ? "D|1|" + this.getDescription() + "|" + this.by : "D|0|" + this.getDescription() + "|" + this.by;
+        return this.isDone ? "D|1|" + this.getDescription() + "|" + this.getDateString(date) : "D|0|" + this.getDescription() + "|" + this.getDateString(date);
     }
 }
