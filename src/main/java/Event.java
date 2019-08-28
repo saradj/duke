ed7b7335e3a50166503ce0aba01290b23b2c411d
saradj <sara.djambazovska@epfl.ch>
2019-08-28 22:48:50 +0800

    fix bug date

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -15,7 +15,7 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + at + ")";
+        return "[E]" + super.toString() + "(at: " + getDateString(date) + ")";
     }
 
     public String printInFile() {
