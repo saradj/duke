322508ef95d55417db091862e29fc902e310f00d
saradj <sara.djambazovska@epfl.ch>
2019-09-08 15:31:54 +0800

    A-JUnit

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,11 +1,16 @@
 import org.junit.jupiter.api.Test;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DukeTest {
     @Test
     public void dummyTest(){
         assertEquals(2, 2);
+    }
+    @Test
+    public void testExitCommand() throws DukeException {
+           Duke duke= new Duke("data/tasks.txt");
+            Command c=Parser.parse("bye");
+         assertTrue(c instanceof ExitCommand);
     }
 }
